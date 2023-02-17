152d90253a9ac2a42b4527a89a151a8c56d591b0
Amol <ajamnik.mech@gmail.com>
2023-02-17 12:56:27 +0530

    [Amol]- Read or Write Address Book With Preson Contact Using File IO

---
+++ FileInputOutPut/FileIO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInputOutPut
+{
+    public class FileIO
+    {
+        public static void WriteRecordsInFile(string path, Contact person)
+        {
+
+            if (File.Exists(path))
+            {
+                StreamWriter sw = File.AppendText(path);
+                sw.WriteLine("\nFirst Name : " + person.FirstNmae );
+                sw.WriteLine("Last Name : " + person.LastName );
+                sw.WriteLine("Address : " + person.Address);
+                sw.WriteLine("City : " + person.City);
+                sw.WriteLine("State : " + person.State);
+                sw.WriteLine("Email : " + person.Email);
+                sw.WriteLine("Zip code : " + person.Zip);
+                sw.WriteLine();
+                sw.Close();
+                Console.WriteLine("\nData added successfully in file");
+            }
+            else
+            {
+                Console.WriteLine("\nFile Not Found");
+            }
+        }
+        public static void ReadRecordsFromFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            else
+            {
+                Console.WriteLine("\nFile Not Found");
+            }
+        }
+
+        
+    }
+}
---
+++ FileInputOutPut/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInputOutPut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\AMD\RFP244\AddressBook_In_Input-Output_File\FileInputOutPut\ReadWriteFile.txt";
+
+            Contact input = new Contact();
+            Console.WriteLine("\nEnter your First Name : ");
+            input.FirstNmae  = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name : ");
+            input.LastName  = Console.ReadLine();
+            Console.WriteLine("Enter your Address : ");
+            input.Address  = Console.ReadLine();
+            Console.WriteLine("Enter your City Name : ");
+            input.City = Console.ReadLine();
+            Console.WriteLine("Enter your State Name : ");
+            input.State = Console.ReadLine();
+            Console.WriteLine("Enter your Zip Code : ");
+            input.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Phone Number : ");
+            input.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your Email Address: ");
+            input.Email = Console.ReadLine();
+
+            
+
+            FileIO.WriteRecordsInFile(path, input);
+            Console.WriteLine("\n\nRecords present in file are : ");
+            FileIO.ReadRecordsFromFile(path);
+            Console.ReadLine();
+        }
+    }
+}
